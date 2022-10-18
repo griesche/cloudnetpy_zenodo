993d7972b956349c3a00c3ea8e8377ff14e58fdc
hannes <griesche@tropos.de>
2022-10-18 08:19:58 +0000

    added kazr converting

--- cloudnetpy/instruments/general.py
+++ cloudnetpy/instruments/general.py
@@ -31,9 +31,11 @@ def add_radar_specific_variables(obj):
     key = "radar_frequency"
     obj.data[key] = CloudnetArray(obj.instrument.frequency, key)
     try:
-        possible_nyquist_names = ("ambiguous_velocity", "NyquistVelocity")
+        possible_nyquist_names = ("ambiguous_velocity", "NyquistVelocity" ,"nyquist_velocity")
         data = obj.getvar(*possible_nyquist_names)
         key = "nyquist_velocity"
+        if max(np.diff(np.array(data))) == 0:  # KAZR stores nyquist velocity as array but has only one sequence and one folding velocity no matter if burst or chirp mode
+            data = data[0]
         obj.data[key] = CloudnetArray(np.array(data), key)
     except RuntimeError:
         logging.warning("Unable to find nyquist_velocity")
